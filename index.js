2fb6751b0d021cbaf4974d12a7e1989115777a45
Evan Gillogley <evanjmg@gmail.com>
2017-11-12 23:24:59 +0000

    Remove unnecessary destructure to fix Jest tests
    This fixes this issue -> https://github.com/tradle/react-native-passcode-status/issues/1

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ import { NativeModules } from 'react-native'
 let RNPS = NativeModules.RNPasscodeStatus
 
 // written out for clarity
-const { supported, status, get } = RNPS
-export default { supported, status, get }
+// { supported, status, get } = RNPS
+export default RNPS
